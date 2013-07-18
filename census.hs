69d35c9455d8bbeb2dc36a03f0b85c87be0d13d7
Eric Kow <eric.kow@gmail.com>
2013-07-18 11:44:55 +0100

    Mechanism to lump together codes; display OTHER
    Also, case-folding

--- census.hs
+++ census.hs
@@ -6,6 +6,7 @@ import qualified Data.ByteString.Lazy as BL
 import           Data.Function        (on)
 import           Data.List
 import qualified Data.Map             as Map
+import           Data.Maybe           (fromMaybe)
 import           Data.Monoid          ((<>))
 import           Data.Ratio
 import qualified Data.Text            as T
@@ -56,11 +57,45 @@ instance FromRecord Answer where
                <*> (parseList <$> v .! 8) -- improvement codes
         | otherwise     = mzero
       where
-        parseList = filter (not . T.null) . map T.strip . T.splitOn ","
+        parseList = map T.toLower
+                  . filter (not . T.null) . map T.strip
+                  . T.splitOn ","
 
 -- ---------------------------------------------------------------------
 --
 -- ---------------------------------------------------------------------
+
+data Lump = Lump
+    { lumpThese :: [T.Text]
+    , lumpInto  :: T.Text
+    }
+  deriving (Show, Eq)
+
+interests :: [Lump]
+interests =
+    [ Lump [ "literature", "watts", "philosophy" ] "philosophy/media"
+    , Lump [ "peace", "suffering"     ] "peace/suffering"
+    , Lump [ "koans", "meditation"    ] "koans/meditation"
+    , Lump [ "non-dual", "suchness", "mu", "attachment", "ego" ] "non-dual/suchness/mu/attachment/ego"
+    , Lump [ "life", "background"     ] "life/background"
+    , Lump [ "approach", "simplicity" ] "approach"
+    , Lump [ "spirituality", "buddhism" ] "buddhism/spirituality"
+    , Lump [ "now"  ]                   "present-moment"
+    , Lump [ "self" ]                   "self-improvement"
+    , Lump [ "more" ]                   "see-details"
+    ]
+
+improvements :: [Lump]
+improvements =
+    [ Lump [ "more" ]                     "see-details"
+    , Lump [ "newbie" ]                   "more-newbie-friendly"
+    , Lump [ "infrastructure", "events" ] "events/infrastructure"
+    ]
+
+lump :: [Lump] -> T.Text -> T.Text
+lump ls t =
+     maybe t lumpInto $
+     find (elem t . lumpThese) ls
 
 process :: V.Vector Answer -> T.Text
 process xs = T.unlines $
@@ -69,12 +104,14 @@ process xs = T.unlines $
     , header "Sanghas (practitioners only)"
     , showCounts aLineages (V.filter aSangha xs)
     , header "Why interested in Zen?"
-    , showCounts aInterestCodes xs
+    , showCounts interest xs
     , header "How to improve this Reddit?"
-    , showCounts aImproveCodes xs
+    , showCounts improve  xs
     ]
   where
     header str = str <> "\n" <> T.replicate (T.length str) "-"
+    interest = map (lump interests)    . aInterestCodes
+    improve  = map (lump improvements) . aImproveCodes
 
 textual :: (Answer -> T.Text) -> V.Vector Answer -> T.Text
 textual f = T.intercalate "\n\n" . V.toList . V.map f
@@ -82,8 +119,13 @@ textual f = T.intercalate "\n\n" . V.toList . V.map f
 showCounts :: (Answer -> [T.Text]) -> V.Vector Answer -> T.Text
 showCounts f xs =
     showMap primary <>
-    showMap (Map.fromList [("OTHER",other)])
+    showMap (Map.fromList [("OTHER",length other)]) <>
+    "\n" <> T.intercalate ", " (map (snippet 10) other)
   where
+    snippet n t =
+        if T.length t <= n
+             then t
+             else T.take n t <> "…"
     (primary, other) = partitionOther (count xs)
     --
     count :: V.Vector Answer -> Map.Map T.Text Int
@@ -106,14 +148,13 @@ percentages :: Int -> Map.Map a Int -> Map.Map a (Int, Rational)
 percentages total xs =
     Map.map (\x -> (x, (fromIntegral x % fromIntegral total))) xs
 
-
 histogram :: Ord a => [a] -> Map.Map a Int
 histogram xs = Map.fromListWith (+) $ zip xs (repeat 1)
 
 -- | Seperate out cases where only one person has given that answer
-partitionOther :: Map.Map T.Text Int -> (Map.Map T.Text Int, Int)
+partitionOther :: Map.Map T.Text Int -> (Map.Map T.Text Int, [T.Text])
 partitionOther m =
-    (plural, Map.size singular)
+    (plural, Map.keys singular)
   where
     (plural, singular) = Map.partition (> 1) m
 
